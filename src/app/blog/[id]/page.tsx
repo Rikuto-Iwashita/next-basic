ffd3bf12c58e139c6b01f478f7b44963897fe600
Rikuto Iwashita <02rikuto18@gmail.com>
2025-06-21 23:15:48 +0900

    セクション2まで進めました

---
+++ src/app/blog/[id]/page.tsx
@@ -0,0 +1,21 @@
+type Params = {
+    params: Promise<{
+        id: string
+    }>
+}
+
+export async function generateMetadata({params} : Params) {
+  const {id} = await params
+  return {
+    title: `ブログ記事ID: ${id}`
+  }
+}
+
+export default async function page({params}: Params) {
+    const {id} = await params
+  return (
+    <div>
+      ブログID: {id}
+    </div>
+  )
+}
